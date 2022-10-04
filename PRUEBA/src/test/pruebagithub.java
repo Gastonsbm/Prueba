01df551d6c786c0e9c6d1b2cdc5f93fde0a4a7cf
Gastón-developer <Gastn-developer@DESKTOP-OBHJ7G7>
2022-10-04 20:17:47 -0300

    SEGUNDO PRINTLINE

--- PRUEBA/src/test/pruebagithub.java
+++ PRUEBA/src/test/pruebagithub.java
@@ -4,6 +4,7 @@ public class pruebagithub {
 
 	public static void main(String[] args) {
 		System.out.println("EL pelotudo de DARWIN y el pela gato del seba ");
+		System.out.println("HOLA");
 
 	}
 
